0bb400d2bb305e345afa5c4b25425352223cc728
ChrisnNg <cristopherng@hotmail.com>
2019-10-17 00:59:34 +0000

    added counts and link to admin/products page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["Basic_Auth_credentials_username"], password: ENV["Basic_Auth_credentials_password"]
   def show
+    @products = Product.all.order(created_at: :desc)
+    @categories = Category.all.order(created_at: :desc)
   end
 end
