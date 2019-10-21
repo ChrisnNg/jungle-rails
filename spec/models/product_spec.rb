4001b9484c852b5f1b4ae01bbadc86b2152a1612
ChrisnNg <cristopherng@hotmail.com>
2019-10-21 21:51:48 +0000

    refined tests for specific error messages

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,22 +13,27 @@ RSpec.describe Product, type: :model do
 
     it "is valid given valid parameters" do
       expect(subject).to be_valid
+      expect(subject.errors.full_messages.first).to eq nil
     end
     it "validates name is present" do
-        subject.name = nil
+      subject.name = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages.first).to eq "Name can't be blank"
     end
     it "validates price is present" do
-        subject.price_cents = nil
+      subject.price_cents = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages.first).to eq "Price cents is not a number"
     end
     it "validates quantity is present" do
-        subject.quantity = nil
+      subject.quantity = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages.first).to eq "Quantity can't be blank"
     end
     it "validates category is present" do
-        subject.category = nil
+      subject.category = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages.first).to eq "Category can't be blank"
     end
 
   end
