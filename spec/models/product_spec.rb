9045b425e935c68320804bf6c99aea69e8621758
ChrisnNg <cristopherng@hotmail.com>
2019-10-21 20:46:51 +0000

    all tests complete

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,22 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    subject { described_class.new }
+
+    subject {
+      described_class.new(
+      name: 'Human Feet Shoes',
+      quantity: 82,
+      price_cents: 224.50,
+      category: Category.new(name: "test cat")
+    )}
 
     it "is valid given valid parameters" do
-      @category = Category.new(name: "test cat")
-
-        subject.name = 'Human Feet Shoes',
-        subject.quantity = 82,
-        subject.price = 224.50,
-        subject.category = @category
-
       expect(subject).to be_valid
     end
-    it "validates name is present"
-    it "validates price is present"
-    it "validates quantity is present"
-    it "validates category is present"
-    # validation tests/examples here
+    it "validates name is present" do
+        subject.name = nil
+      expect(subject).to_not be_valid
+    end
+    it "validates price is present" do
+        subject.price_cents = nil
+      expect(subject).to_not be_valid
+    end
+    it "validates quantity is present" do
+        subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+    it "validates category is present" do
+        subject.category = nil
+      expect(subject).to_not be_valid
+    end
+
   end
 end
